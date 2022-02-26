927620d98f47c55c7de5bec5ef75441caa2c5c15
yolocka <yolocka@bk.ru>
2022-02-27 01:37:32 +0300

    Homework for lesson 5

--- app/src/main/java/com/example/mypictureoftheday/view/MainActivity.kt
+++ app/src/main/java/com/example/mypictureoftheday/view/MainActivity.kt
@@ -1,28 +1,27 @@
 package com.example.mypictureoftheday.view
 
 import android.annotation.SuppressLint
+import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebSettings
+import android.view.*
 import android.webkit.WebViewClient
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.*
 import coil.load
 import com.example.mypictureoftheday.R
 import com.example.mypictureoftheday.model.PictureData
 import com.example.mypictureoftheday.view.archive.ArchiveActivity
 import com.example.mypictureoftheday.viewmodel.MainViewModel
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.collapsing_toolbar.*
@@ -36,15 +35,16 @@ class MainActivity : AppCompatActivity() {
     private val editor: SharedPreferences.Editor by lazy {
         sharedPref.edit()
     }
-
+    private var toMoveFABAnimation = false
+    private var isExpanded = false
 
     companion object {
         const val PREF_THEME = "app_theme"
         const val IS_CHANGED = "is_changed"
         const val SHAR_PREF_NAME = "my_pref"
         const val PLUM_THEME = "Слива"
-        const val MINT_THEME = "Мята"
-        const val ORANGE_THEME = "Апельсин"
+        const val MANDARIN_THEME = "Мандарин"
+        const val BLUEBERRY_THEME = "Черника"
         const val MEDIA_TYPE_VIDEO = "video"
         const val MEDIA_TYPE_IMAGE = "image"
     }
@@ -53,8 +53,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         when (sharedPref.getString(PREF_THEME, "")) {
             PLUM_THEME -> setTheme(R.style.PlumTheme)
-            MINT_THEME -> setTheme(R.style.MintTheme)
-            ORANGE_THEME -> setTheme(R.style.OrangeTheme)
+            MANDARIN_THEME -> setTheme(R.style.MandarinTheme)
+            BLUEBERRY_THEME -> setTheme(R.style.BlueberryTheme)
         }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -63,6 +63,26 @@ class MainActivity : AppCompatActivity() {
 
         setBottomAppBar()
 
+        image_view.setOnClickListener {
+            isExpanded = !isExpanded
+            TransitionManager.beginDelayedTransition(
+                main_activity,
+                ChangeImageTransform()
+            )
+            val params: ViewGroup.LayoutParams = image_view.layoutParams
+            params.height =
+                if (isExpanded) main_activity.height else ViewGroup.LayoutParams.MATCH_PARENT
+            image_view.layoutParams = params
+        }
+
+        my_fab.setOnClickListener {
+            toMoveFABAnimation = !toMoveFABAnimation
+            val params = my_fab.layoutParams as CoordinatorLayout.LayoutParams
+            params.anchorGravity =
+                if (toMoveFABAnimation) Gravity.END or Gravity.BOTTOM or Gravity.LEFT
+                else Gravity.END or Gravity.BOTTOM or Gravity.RIGHT
+            my_fab.layoutParams = params
+        }
     }
 
     override fun onResume() {
@@ -91,10 +111,10 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.item_settings -> {
-                startActivity(Intent(this, SettingsActivity::class.java))
+                startActivity(Intent(this, SettingsActivity::class.java), ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
             }
             R.id.item_archive -> {
-                startActivity(Intent(this, ArchiveActivity::class.java))
+                startActivity(Intent(this, ArchiveActivity::class.java), ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
             }
             android.R.id.home -> {
                 BottomNavigationFragment().show(supportFragmentManager, "tag")
